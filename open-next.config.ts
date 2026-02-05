8b40e06c52c8ed19c1c37f83fd59df76e3a7ac13
halukinal <inlhalk@gmail.com>
2026-02-05 11:54:50 +0300

    fix: update open-next config with cloudflare overrides

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,11 @@
 const config = {
     default: {
         minify: true,
+        override: {
+            wrapper: "cloudflare-node",
+            converter: "edge",
+            proxyExternalRequest: "fetch",
+        },
     },
 };
 
